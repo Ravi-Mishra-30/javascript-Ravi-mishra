3f9df62d4d13d805d73f39af3fe1c3c42969411b
Ravi-Mishra-30 <ravimishra6386689@gmail.com>
2025-02-12 11:10:24 +0000

    dataTypes

---
+++ besics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // yreat all JS code as newer version
+
+// alert(3 + 3) we are using node.js not browser
+
+// console.log(3 
+//     + 2)  // code readbility should be high
+
+console.log("Mishra Ravi Shivshankar");
+
+
+let name = "Ravi"
+let age = 21
+let isLoggedIn = false
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+// object
+
+// typeof  // it is giving the type of the variable
+console.log(typeof "Ravi")
+
+console.log(typeof "null") // null
+console.log(typeof "undifined")  //undefined
+
+
+
