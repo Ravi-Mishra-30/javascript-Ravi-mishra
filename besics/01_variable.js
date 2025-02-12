cc4f3b2e99ba54f91d35996f4fe4767f1adf03ff
Ravi-Mishra-30 <ravimishra6386689@gmail.com>
2025-02-12 10:48:28 +0000

    container config don

---
+++ besics/01_variable.js
@@ -0,0 +1,29 @@
+const accountId = 40014 // const can not be change it will be save automatic
+let accountEmail = "ravimishra6386689@gmail.com"
+var accountPassword = "638668"
+accountCity = "jaupur"
+
+// accountId = 40011 //not allowed
+
+accountEmail = "ravimishra63216@gmail.com"
+
+var accountPassword = "123456"
+
+accountCity = "Vadodara"
+
+let accountState;
+
+// console.log(accountId);
+
+// console.log(accountEmail);
+
+// console.log(accountPassword);
+
+// console.log(accountCity);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
