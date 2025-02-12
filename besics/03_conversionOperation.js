8f315507ff30d8f352b29e3c208b51148388d85a
Ravi-Mishra-30 <ravimishra6386689@gmail.com>
2025-02-12 11:32:08 +0000

    conversion of datatypes

---
+++ besics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let score = null
+
+// console.log(typeof score); //or
+// console.log(typeof(score));
+
+
+// let valueInNumber = Number(score) //string to number conversation
+
+// console.log(typeof(valueInNumber));
+
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+// let isLoggedIn = "Ravi Mishra"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Ravi Mishra" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
