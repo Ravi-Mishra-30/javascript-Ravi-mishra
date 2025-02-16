fd1e1d2b62aa022c44930b3433bcaf9d0e4ed120
Ravi-Mishra-30 <ravimishra6386689@gmail.com>
2025-02-16 18:10:51 +0000

    string

---
+++ besics/07_string.js
@@ -0,0 +1,44 @@
+
+
+// String
+
+const name = "Ravi Mishra"
+const repoCount = 50 
+// console.log(name + repoCount + " value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Ravi-Mishra');
+
+// console.log(gameName[0]); // for index number
+
+// console.log(gameName.__proto__); // for prototype
+
+// console.log(gameName.length); // for length
+
+// console.log(gameName.toLowerCase());
+
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(5));
+
+// console.log(gameName.indexOf('a'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+// const newStringOne = "       Ravi   "
+// console.log(newStringOne);;
+// console.log(newStringOne.trim());
+
+
+// const url = "https://ravi.com/ravi%20mishra"
+
+// console.log(url.replace('%20', '-'));
+
+// console.log(url.includes('ravi')); 
+
+console.log(gameName.split('-'));
