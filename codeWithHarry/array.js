6c6888a866e77f16053a6cd46b96c80604e8fad7
Ravi-Mishra-30 <ravimishra6386689@gmail.com>
2025-03-20 12:14:52 +0000

    array

---
+++ codeWithHarry/array.js
@@ -0,0 +1,31 @@
+let arr = [1, 2, 3, 4, 6, 7, 9]
+// console.log(arr, typeof arr) // type of arr is object
+// console.log(arr.length)// give me the length of all the array
+// console.log(arr[5]) // give me the index value of one element
+
+// arr[0] = 5146541; //changing the index value
+// console.log(arr[0]);
+
+
+// tostring() method
+
+// console.log(arr.toString())
+
+// join() method
+
+// console.log(arr.join(" and "))
+
+// pop() method 
+
+// console.log(arr.pop()); //it will be delete the last element
+// console.log(arr);
+
+// push() method
+
+// console.log(arr.push(5)) // it will be add last element
+// console.log(arr);
+
+
+// console.log(arr.shift()) //it will be remove the first element
+// console.log(arr)
+// console.log(arr.length)
