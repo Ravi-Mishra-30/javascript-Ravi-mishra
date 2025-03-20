d8e30df830648432cbb7bbc42dd69afc810e6efb
Ravi-Mishra-30 <ravimishra6386689@gmail.com>
2025-03-20 16:06:48 +0000

    array

--- codeWithHarry/array.js
+++ codeWithHarry/array.js
@@ -1,3 +1,4 @@
+/* 
 let arr = [1, 2, 3, 4, 6, 7, 9]
 // console.log(arr, typeof arr) // type of arr is object
 // console.log(arr.length)// give me the length of all the array
@@ -34,6 +35,26 @@ let arr = [1, 2, 3, 4, 6, 7, 9]
 
 // unshift() method
 
-console.log(arr.unshift(10, 20, "Harry")) // it will be add one the element at the first
+// console.log(arr.unshift(10, 20, "Harry")) // it will be add one the element at the first
+// console.log(arr)
+
+// delete method
+
+console.log (delete arr[3])
 console.log(arr)
+console.log(arr[3])
+
+*/
+
+// concat method : it is used  to jjoin to the given array
+
+let arr1 = [1, 2, 3, 4]
+let arr2 = [5, 6, 7, 8]
+let arr3 = [9, 10, 11, 12]
+
+console.log(arr1.concat(a2, a3))
+
+
+
+
 
